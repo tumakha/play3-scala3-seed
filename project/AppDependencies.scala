50cbceffb285775e775206592b6ab27a1deae458
Scala Steward <scala_steward@virtuslab.com>
2025-04-08 23:44:08 +0000

    Update cats-effect to 3.6.1

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val catsEffectVersion         = "3.6.0"
+  private val catsEffectVersion         = "3.6.1"
   private val apachePOIVersion          = "5.4.1"
   private val pdfBoxVersion             = "3.0.4"
   private val pdfItext5Version          = "9.7.2"
